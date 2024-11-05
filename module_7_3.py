dfa517c2000d5ccd81abf9c6b9ad99e8dc1fae2d
Katya1985 <berkat85@rambler.ru>
2024-11-05 12:04:14 +0300

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -2,39 +2,66 @@ import io
 from pprint import pprint
 name = 'test_file.txt'
 
-class WordsFinder: # WordsFinder('file1.txt, file2.txt', 'file3.txt', ...)
+
+# Задача "Найдёт везде"
+
+class WordsFinder:
+
+
     def __init__(self, *file_names):
         self.file_names = file_names
 
 
-    def get_all_words(self): # подготовительный метод, который возвращает словарь
+    def get_all_words(self):
         all_words = {}
-        for file_name in self.file_names:
-            list = []
-            with open(file_name, encoding='utf-8') as file:
-                for line in file:
-                    line_cls = line.lower().replace(',', '').replace('.', '').replace('=', '').replace('!', '').replace(
-                        '?', '').replace(';', '').replace(':', '').replace('-', '').split()
-                    list.append(line_cls)
-                all_words[self.file_names] = sum(list, [])
-                return all_words
+        # названия файлов : слова содержащиеся в этом файле
+        for file_names in self.file_names:
+            with open(file_names, encoding = 'utf-8') as file:
+                # перевод в нижний регистр стоки переменной file
+                file = file.read().lower()
+                for i in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    # подмена найденных знаков на пустое значение (избавляемся от знаков) в строке
+                    file = file.replace(i, '')
+                    # разбиение строки на элементы списка методом split()
+                    words = file.split()
+                    # присвоение словарю all_words ключу (file_name) значения списка слов (words)
+                    all_words[file_names] = words
+            # возврат словаря метода get_all_words()
+            return all_words
+
+# find(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла,
+# значение - позиция первого такого слова в списке слов этого файла
     def find(self, word):
-        find_dict = {}
-
-        for name, words in self.get_all_words().items():
-            if word in words:
-                find_dict[name] = words.index(word.lower()) + 1
-            return find_dict
-
-    def count(self, word):
-        all_words = self.get_all_words()
+        # # объявление пустого словаря result
         result = {}
-        for file_names, words in all_words.items():
-            result[file_names] = words.count(word)
-        return result
+        # цикл перебора в файле (i) слов (word_) из словаря all_words из метода get_all_words
+        for i, word_ in self.get_all_words().items():
+            # цикл перебора (поиска) номера (j) слова (wd) из списка word_ начиная c 1
+            for j, wd in enumerate(word_, 1):
+                # условие сравнения слова (wd) и приведенного к нижнему регистру принятого
+                # (искомого) слова (word)
+                if wd == word.lower():
+                    # присвоение переменной result[i] найденного номера
+                    result[i] = j
+                    # выход из условия при нахождении первого тождества
+                    break
+                # возврат метода find()
+                return result
 
+        # объявление метода count(self, word). Возвращает словарь, где ключ - название файла,
+        # значение - количество слова word в списке слов этого файла
+    def count(self, word):
+        # объявление пустого словаря result_
+        result_ = {}
+        # цикл перебора в файле (i) слов (word_) из словаря all_words из метода get_all_words
+        for i, word_ in self.get_all_words().items():
+            # присвоение словарю result_[i] количества совпадений word_ в приведенном к
+            # нижнему регистру искомого (принятого) слова word
+            result_[i] = word_.count(word.lower())
+        # возврат метода count()
+        return result_
 if __name__ == '__main__':
     finder2 = WordsFinder('test_file.txt')
-    print(finder2.get_all_words())  # Все слова
-    print(finder2.find('TEXT'))  # 3 слово по счёту
-    print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+    print(finder2.get_all_words()) # Все слова
+    print(finder2.find('TEXT')) # 3 слово по счёту
+    print(finder2.count('teXT')) # 4 слова teXT в тексте всего
